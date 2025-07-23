b0982bb6ed49e52e996457aed649f46cc6f71145
Iuri Guilherme Bogar Portilho <iuribogar@hotmail.com>
2025-07-23 16:52:04 -0300

    complete version of the page

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
+
+
+function compileSass() {
+    return gulp.src('./source/styles/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist/styles'));
+}
+
+function minifyImg() {
+    return gulp.src('./source/images/**/*.{jpg,jpeg,png,webp}')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+function watchFiles() {
+    gulp.watch('./source/styles/*.scss', compileSass);
+    gulp.watch('./source/images/*', minifyImg);
+}
+
+exports.default = gulp.series(
+    compileSass,
+    minifyImg,
+    watchFiles
+);
